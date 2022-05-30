1a9157dccf788e7db34c77e5c50a740171d5baed
bahariandikoo <bahariandikoo@gmail.com>
2022-05-30 21:12:16 +0700

    Update UI Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
     color: white;
    }
 </style>
-    <div class="row" style="margin-top: 10%">
-        <div class="col-md-10">
+    <div class="row" >
+        <div class="col-md-12">
             <div id="bungkus">
-                <div class="kartu"><a style="text-decoration: none;" href="carakan">Carakan</a></div>
-                <div class="card"><a  href="aksaraangka">Aksara Angka</a></div>
-                <div class="card"><a  href="aksaramurda">Aksara Murda</a></div>
-                <div class="card"><a  href="sandhangan">Sandhangan</a></div>
-                <div class="card"><a  href="aksararekan">Aksara Rekan</a></div>
-                <div class="card"><a  href="pasangan">Pasangan</a></div>
+                <div class="kartu">
+                    <a  id='kartu-link' style="">Aksara Angka</a></div>
+                <div class="kartu"><a id='modul2' href="aksaramurda">Aksara Murda</a></div>
+                <div class="kartu"><a  href="sandhangan">Sandhangan</a></div>
+                <div class="kartu"><a  href="aksararekan">Aksara Rekan</a></div>
+                <div class="kartu"><a  href="pasangan">Pasangan</a></div>
             </div>
         </div>
     </div>
